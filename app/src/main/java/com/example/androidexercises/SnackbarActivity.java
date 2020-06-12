737eea21447969ff5afa7aa68edb7cf548ac6b16
alejandro.martinez <alejandro.martinezj@dominion.mx>
2020-06-12 17:41:48 -0500

    Correccion de IntentsActivity.java para las intenciones de envio de correo y abrir sitio web Creacion de actividad SnackbarActivity Creacion de dependencia para uso de coordinatorlayout
    Implementacion de snackbars simple y con accion

--- app/src/main/java/com/example/androidexercises/SnackbarActivity.java
+++ app/src/main/java/com/example/androidexercises/SnackbarActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class SnackbarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    View coordinatorLayOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snackbar);
+
+         coordinatorLayOut = findViewById(R.id.cltContent_Snack);
     }
 
 
@@ -18,10 +24,23 @@ public class SnackbarActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnSimple_Snackbar:
+                Snackbar.make(coordinatorLayOut, "Snackbar básico", Snackbar.LENGTH_LONG).show();
                 break;
 
             case R.id.btnCustom_Snackbar:
+                Snackbar.make(coordinatorLayOut, "Snackbar personalizado", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                break;
 
+            case R.id.btnAction_Snackbar:
+                Snackbar.make(coordinatorLayOut, "Snackbar con accion", Snackbar.LENGTH_SHORT).
+                    setAction("Undo", new View.OnClickListener(){
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar undo = Snackbar.make(coordinatorLayOut, "Deshacer", Snackbar.LENGTH_SHORT);
+                            undo.show();
+                        }
+                    }).show();
+                break;
         }
     }
 }
